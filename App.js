6fdd4db7c400e55bab4b8d299373b7f8f49f11f2
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-24 21:20:05 -0500

    added nav dependencies

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-rn';
 
 export default function App() {
   return (
     <View style={tw("flex-1 justify-center items-center")}>
       <Text>Test!</Text>
+      <Button title="click me!"></Button>
     </View>
+
   );
 }
 
