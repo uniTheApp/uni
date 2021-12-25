1e0e65257cd7e5a948543caad4b176108a3f1662
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-25 15:26:53 -0500

    firebase non-code ready

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,13 +1,24 @@
 import React, { createContext, useContext } from 'react'
-import { View, Text } from 'react-native';
+import * as Google from "expo-google-app-auth"
 
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
+
+    // may have issue with android in future
+    // expo build:android did not let change name to expo hosting
+    // around ~1:09:00 in tut vid
+    // so may not be able to use expo on android, but will 
+    // be able to in future when we change apk package name
+    const signInWithGoogle = async () => {
+
+    }
+
+
     return (
     <AuthContext.Provider 
         value={{
-            user: "Kyle",
+            user: null,
         }}
         
     >        
