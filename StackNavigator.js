76235a28b9ee828017262192c8cc15f1b8f5f892
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-24 22:53:00 -0500

    basic home screen and chat screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +11,8 @@ const StackNavigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+            <Stack.Screen name="Chat" component={ChatScreen}></Stack.Screen>
+        
         </Stack.Navigator>
     )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
     return (
         <View>
-            <Text>"I am the home screen"</Text>
+            <Text>I am the home screen</Text>
+            <Button title="Go to the Chat Screen" onPress={() => navigation.navigate("Chat")}></Button>
         </View>
     )
 }
