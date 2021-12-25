76a0f0db6486384639499ac2456e896518d0bea2
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-24 21:29:58 -0500

    basic stack

---
+++ StackNavigator.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Stack = createNativeStackNavigator();
+
+
+const StackNavigator = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
