82198d4af0bcf75fcd37ea465f29e0e4028eea44
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-27 00:03:45 -0500

    about to add header

--- StackNavigator.js
+++ StackNavigator.js
@@ -17,7 +17,11 @@ const StackNavigator = () => {
     const { user } = useAuth();
 
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             { user ? (
                 <>
                     <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { View, Text, Button} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react';
+import { View, Text, Button, SafeAreaView} from 'react-native';
 import useAuth from '../hooks/useAuth';
     // ES6 deconstrution operator
 const LoginScreen = () => {
 
     const { signInWithGoogle, loading } = useAuth();
+    const navigation = useNavigation()
+
 
     return (
-        <View>
+        <SafeAreaView>
+            {/* header */}
+
+
+
+            {/* end header */}
             <Text>{ loading ? 'loading...' : "Please Login"}</Text>
             <Button title="login" onPress={signInWithGoogle}/>
-        </View>
+        </SafeAreaView>
     )
 }
 
