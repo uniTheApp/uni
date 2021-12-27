65f47f85a48d17bc8761a036c3a231d299c8d856
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-12-27 14:54:44 -0500

    added basic email and pass

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useLayoutEffect } from 'react';
-import { View, Text, Button, SafeAreaView, Touchable} from 'react-native';
+import { View, Text, TextInput, Button, SafeAreaView, KeyboardAvoidingView, secureTextEntry, StyleSheet, Touchable} from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import useAuth from '../hooks/useAuth';
     // ES6 deconstrution operator
 const LoginScreen = () => {
 
     const { signInWithGoogle, loading } = useAuth();
     const navigation = useNavigation()
+    
+
+
 
 
     return (
-        <SafeAreaView>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding">
             {/* header */}
             {/* <TouchableOpacity>
                 <Text>Login</Text>
@@ -22,10 +28,44 @@ const LoginScreen = () => {
 
 
             {/* end header */}
+
+            {/* body */}
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Email"
+                    // value={} 
+                    // onChangeText={text => }
+                    style = {styles.input}
+                >
+                </TextInput>
+                <TextInput
+                    placeholder="Password"
+                    // value={} 
+                    // onChangeText={text => }
+                    style = {styles.input}
+                    secureTextEntry
+                >
+                </TextInput>
+
+            </View>
+
+
+            {/* end body */}
+
             <Text>{ loading ? 'loading...' : "Please Login"}</Text>
             <Button title="login" onPress={signInWithGoogle}/>
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
