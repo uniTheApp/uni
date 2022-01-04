951accde28146044c4b0a6d636c1f618c7105e1d
Kyle Andruczk <kyle.andruczk@gmail.com>
2022-01-03 19:34:37 -0500

    debug

--- screenComponents/TopBar.js
+++ screenComponents/TopBar.js
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 
 import React from 'react'
 import { View, Text, Button, SafeAreaView, StyleSheet, Image, FlatList, Dimensions, TouchableOpacity } from 'react-native'
