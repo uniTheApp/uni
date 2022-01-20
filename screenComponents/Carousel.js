06e2433dcb05e5d30bd0e6fc6c655175b40c0e2d
Kyle Andruczk <kyle.andruczk@gmail.com>
2022-01-20 15:36:46 -0500

    up thru carousel

--- screenComponents/Carousel.js
+++ screenComponents/Carousel.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Dimensions, FlatList, Animated } from 'react-native'
 import CarouselItem from './CarouselItem'
 
-
-
-
-const { width, heigth } = Dimensions.get('window')
+const { width } = Dimensions.get('window')
 
 
 const Carousel = ({ data }) => {
+    // get the 
     const scrollX = new Animated.Value(0)
     let position = Animated.divide(scrollX, width)
     const [dataList, setDataList] = useState(data)
@@ -22,9 +20,10 @@ const Carousel = ({ data }) => {
     if (data && data.length) {
         return (
             <View>
-
+                {/* view for the dots above the screen in carosel */}
                 <View style={styles.dotView}>
-                    {data.map((_, i) => {
+                    {/* dynamically change opacity of dots */}
+                    {data.map((_, i) => { 
                         let opacity = position.interpolate({
                             inputRange: [i - 1, i, i + 1],
                             outputRange: [0.3, 1, 0.3],
@@ -40,6 +39,7 @@ const Carousel = ({ data }) => {
 
                 </View>
                 
+                {/* horizontal flatlist of all images */}
                 <FlatList data={data}
                 ref = {(flatList) => {this.flatList = flatList}}
                     keyExtractor={(item, index) => 'key' + index}
@@ -62,8 +62,6 @@ const Carousel = ({ data }) => {
             </View>
         )
     }
-
-    console.log('Please provide Images')
     return null
 }
 
